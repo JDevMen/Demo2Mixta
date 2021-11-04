b9e8d68b93fdfbd0e8ea8da60c001a09a50586bb
Julian Mendoza Ruiz <jdmendoza_90@hotmail.com>
2021-11-04 16:17:27 -0500

    test boton

---
+++ Assets/Scripts/buttonTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonTest : MonoBehaviour
+{
+    public void debugButton()
+    {
+        Debug.Log("Botton seleccionado");
+    }
+}
